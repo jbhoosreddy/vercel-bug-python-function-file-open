114143f03de8788547caad467a5ac19fe30145bd
jbhoosreddy <jbhoosreddy@bloomberg.net>
2020-07-07 23:24:15 -0400

    testing

--- api/python.py
+++ api/python.py
@@ -3,17 +3,18 @@ app = Flask(__name__)
 
 PATH = "file.txt"
 
+@app.route('/api/not-working')
+def not_working():
+    handle = open(PATH, 'r')
+    data = handle.readlines()
+    return data
+
+@app.route('/api/working')
+def working():
+    return send_file(PATH)
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
-
-# @app.route('/api/not-working')
-# def not_working():
-#     handle = open(PATH, 'r')
-#     data = handle.readlines()
-#     return data
-
-# @app.route('/api/working')
-# def working():
-#     send_file(PATH)
